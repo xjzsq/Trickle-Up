5d545a48a9b81915a8e0921d83a6f18c26ca76ae
xjzsq <sdxjzsq@126.com>
2020-04-06 20:01:34 +0800

    233

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { View,Drawer, Container, DeckSwiper, Header, Title, Content, Footer, FooterTab, Thumbnail, Button, Left, Right, Body, Icon, Text, Card, CardItem, } from 'native-base';
+import { Image, Dimensions, StyleSheet } from 'react-native';
+import { Container, Text, View, DeckSwiper, Header, Title, Content, Footer, 
+   FooterTab, Thumbnail, Button, Left, Right, Body, Icon, Card, CardItem,
+    } from 'native-base';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { AsyncStorage } from '@react-native-community/async-storage';
+import Carousel from 'react-native-snap-carousel';
+import { scrollInterpolator, animatedStyles } from './utils/animations';
 
+/* about screen */
 const cards = [
   {
     text: 'Card One',
@@ -70,30 +75,49 @@ function AboutScreen ({navigation}) {
     );
 }
 
-class HomeScreen extends Component {
+/* 主屏幕卡片 */
+const SLIDER_WIDTH = Dimensions.get('window').width;
+const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.8);
+const ITEM_HEIGHT = Math.round(ITEM_WIDTH * 3 / 4);
+const DATA = [];
+for (let i = 0; i < 10; i++) {
+  DATA.push(i)
+}
+export class HomeScreen extends Component {
   state={
-    HappyThings:{
-      '4.3':{
-        '今晚吃鸡':{
-          type : 'game',
-          val : 2,
-        },
-        '下午没课':{
-          type : 'school',
-          val : 2,
-        },
-      },
-      '4.4':{
-        '不用上课':{
-          type : 'school',
-          val : 3,
-        },
-        '祭祀祖先':{
-          type : 'home',
-          val : 3,
-        },
-      },
-    },
+    index : 0 ,
+    HappyThings:[
+      [
+        '4.3',
+        [
+          {
+            name : '今晚吃鸡', 
+            type : 'game',
+            val : 2,
+          },
+          {
+            name : '下午没课',
+            type : 'school',
+            val : 2,
+          },
+        ],
+      ],
+      [
+        '4.4',
+        [
+          {
+            name : '不用上课', 
+            type : 'school',
+            val : 3,
+          },
+          {
+            name : '祭祀祖先',
+            type : 'home',
+            val : 3,
+          },
+        ],
+      ],
+    ],
     Type:{
       'game' : {
         name : '游戏',
@@ -112,9 +136,28 @@ class HomeScreen extends Component {
       },
     },
   }
-  getTypeName(happyType){
-    alert(this.state.Type.happyType.name);
-    return this.state.Type.happyType.name;
+  constructor(props) {
+    super(props);
+    this._renderItem = this._renderItem.bind(this)
+  }
+  _renderItem({ item }) {
+    return (
+      <View>
+        <Card style={{ elevation: 2 }}>
+          <CardItem style={styles.itemContainer}>
+            <Left>
+              <Body>
+                <Text>{item[0]}</Text>
+              </Body>
+            </Left>
+          </CardItem>
+          <CardItem style={styles.itemContainer}>
+            <Icon name="heart" style={{ color: '#ED4A6A' }} />
+            <Text>{item[1][0].name}</Text>
+          </CardItem>
+        </Card>
+      </View>
+    );
   }
   render(){
     const {navigation} = this.props;
@@ -139,45 +182,52 @@ class HomeScreen extends Component {
           </Right>
         </Header>
         <View>
-          <DeckSwiper
-          dataSource={cards}
-          renderItem={item =>
-            <Card style={{ elevation: 3 }}>
-              <CardItem>
-                <Left>
-                  <Thumbnail source={item.image} />
-                  <Body>
-                    <Text>{item.text}</Text>
-                    <Text note>NativeBase</Text>
-                  </Body>
-                </Left>
-              </CardItem>
-              <CardItem cardBody>
-                <Image style={{ height: 300, flex: 1 }} source={item.image} />
-              </CardItem>
-              <CardItem>
-                <Icon name="heart" style={{ color: '#ED4A6A' }} />
-                <Text>{item.name}</Text>
-              </CardItem>
-            </Card>
-          }
+          <Text style={styles.counter}
+          >
+            {this.state.index}
+          </Text>
+          <Carousel
+            ref={(c) => this.carousel = c}
+            data={this.state.HappyThings}
+            renderItem={this._renderItem}
+            sliderWidth={SLIDER_WIDTH}
+            itemWidth={ITEM_WIDTH}
+            containerCustomStyle={styles.carouselContainer}
+            inactiveSlideShift={0}
+            onSnapToItem={(index) => this.setState({ index })}
+            scrollInterpolator={scrollInterpolator}
+            slideInterpolatedStyle={animatedStyles}
+            useScrollView={true}          
           />
-          {/*
-          <Card>
-            <CardItem>
-              <Body>
-                <Text>
-                   咕咕咕 
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
-          */}
         </View>
       </Container>
     );
   }
 }
+const styles = StyleSheet.create({
+  carouselContainer: {
+    marginTop: 10
+  },
+  itemContainer: {
+    //width: ITEM_WIDTH,
+    height: ITEM_HEIGHT,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  itemLabel: {
+    color: 'white',
+    fontSize: 24
+  },  
+  counter: {
+    marginTop: 25,
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+});
+
+/* 导航插件 */
 
 const DrawerNav = createDrawerNavigator();
 
